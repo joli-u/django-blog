c6549b6ddc72c7c9444790c85116962b0dd6ad14
Joli Umetsu <joliumetsu@gmail.com>
2020-07-03 17:24:35 -0700

    Completed assignment 7 / reversed category and post relationship.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+@admin.register(Category)
+class PostCategory(admin.ModelAdmin):
+    exclude = ('posts',)
+
